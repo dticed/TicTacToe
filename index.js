edbc33d675738dcdb8aa7a961ef990123e9cd138
dticed <lucaspierrealencar@gmail.com>
2021-07-08 16:06:12 -0300

    add winner, fix status, fix restart

--- index.js
+++ index.js
@@ -41,14 +41,13 @@ const displayController = (() => {
 
     fieldElement.forEach(element => {
         element.addEventListener('click', function () {
-            if(gameController.getRound() >= 8) {
+            if (gameController.getRound() >= 8 || gameController.getIsOver()) {
+                console.log(element.getAttribute('data-index'));
                 console.log("meuzovo")
                 boardGameElement.disabled = true;
+                return;
             }
-            console.log(element.getAttribute('data-index'));
             gameController.play(element.getAttribute('data-index'));
-            updateStatus(gameController.getRound())
-            changePlayer();
             updateGameBoard();
         })
     })
@@ -63,34 +62,38 @@ const displayController = (() => {
 
     function updateStatus(round) {
         statusElement.innerHTML = `Round ${round}/9`
+        if()
     }
 
 
     restartElement.addEventListener('click', function () {
-    for (let i = 0; i < fieldElement.length; i++) {
-        gameBoard.setField(i, "");
-        playingElement.innerHTML = "X"
-        gameController.resetGame();
-    }
-    updateGameBoard();
-})
+        for (let i = 0; i < fieldElement.length; i++) {
+            gameBoard.setField(i, "");
+            playingElement.innerHTML = "X"
+            gameController.resetGame();
+        }
+        playingElement.innerHTML = "O";
+        updateGameBoard();
+    })
 
-const updateGameBoard = () => {
-    for (let i = 0; i < fieldElement.length; i++) {
-        fieldElement[i].innerHTML = gameBoard.getField(i);
-    }
-};
-
-updateGameBoard()
-
+    const updateGameBoard = () => {
+        updateStatus(gameController.getRound());
+        changePlayer();
+        for (let i = 0; i < fieldElement.length; i++) {
+            fieldElement[i].innerHTML = gameBoard.getField(i);
+        }
+    };
 })();
 
 const gameController = (() => {
     const playerX = Player('X')
     const playerO = Player('O');
     let round = 0;
+    let isOver = false;
+    let last = "";
 
     function play(index) {
+        checkWins();
         if (round % 2 === 0 && gameBoard.getField(index) === "") {
             gameBoard.setField(index, playerX.getSign());
             round += 1;
@@ -100,30 +103,49 @@ const gameController = (() => {
             round += 1;
             return round;
         }
+        
     };
 
     const getRound = () => {
         return round;
     }
 
+    const getIsOver = () => {
+        return isOver;
+    }
+
     const resetGame = () => {
         round = 0;
     }
 
-    const checkWinner = () => {
-        const wins = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6],
-        ]
-    };
+    const wins = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
 
-    return { play, resetGame, getRound };
+    function checkWins() {
+        for (let i = 0; i < wins.length; i++) {
+            let string = ""
+            for (let j = 0; j < wins[i].length; j++) {
+                string = string.concat(gameBoard.getField(wins[i][j]))
+            }
+            if (string === "XXX") {
+                console.log("X ganhou")
+                isOver = true;
+            } else if (string === "OOO") {
+                console.log("O ganhou")
+                isOver = true;
+            } 
+        }
+    }
+
+    return { play, resetGame, getRound, getIsOver };
 })();
 
 
